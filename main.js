953918ece028e89c64360b76ec6c062617c9f081
Thiago Lorhan <thiagolorhandasilva12@gmail.com>
2025-06-17 22:11:49 -0300

    replace console.log with logError function

--- main.js
+++ main.js
@@ -3,28 +3,26 @@ const avatarInput = document.getElementById("avatarInput");
 const avatarPreview = document.querySelector("#avatarDropzone img");
 const avatarIcon = document.querySelector(".avatar-upload__icon");
 
+function logError(message, ...args) {
+  console.log(`[ERROR] [${new Date().toISOString}: ${message}]`, ...args);
+}
+
 avatarDropzone.addEventListener("drop", (e) => {
   e.preventDefault();
 
-  console.log("processo em andamento");
-
   const reader = new FileReader();
   const file = e.dataTransfer.files[0];
-  console.log("file:", file, "name:", file.name);
-  console.log("type:", file.type, "size:", file.size);
 
   if (file.type.startsWith("image/")) {
     reader.onloadend = () => {
       avatarPreview.src = reader.result;
-      console.log("resultado", reader.result);
     }
     reader.onerror = () => {
-      console.log("Deu erro pai")
+      logError("Error rendering image", reader.error);
     }
 
     reader.readAsDataURL(file);
-    console.log("executando...")
   } else {
-    console.error("Deve ser do tipo imagem");
+    logError("The file must be of image type");
   }
 });
